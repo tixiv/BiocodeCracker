1f4b1d7c1c2c1f7db1a6558de22416466edc0894
Peter Fuhrmann <tixiv@gmx.net>
2025-03-22 13:59:31 +0100

    Implement heat, Make float menu option nicer

--- Source/BiocodeCracker/ThingComp_CompFloatMenuOptions_Patch.cs
+++ Source/BiocodeCracker/ThingComp_CompFloatMenuOptions_Patch.cs
@@ -24,16 +24,27 @@ namespace Tixiv_BiocodeCracker
             // Get all buildings on the map
             var allCrackers = Find.CurrentMap.listerThings.AllThings.OfType<BiocodeCrackerBuilding>();
 
+            // Try to return an empty, powered cracker that is unreserved
             foreach (var cracker in allCrackers)
-            {
-                if (cracker.Empty && cracker.PowerOn)
+                if (cracker.Empty && cracker.PowerOn && !cracker.Map.reservationManager.IsReserved(cracker))
                     return cracker;
-            }
+            
+            // Fall back to return any cracker that is empty and not reserved, we will give an appropriate menu entry for what is wrong later
+            foreach (var cracker in allCrackers)
+                if (cracker.Empty && !cracker.Map.reservationManager.IsReserved(cracker))
+                    return cracker;
+
+            // Fall back to return any empty cracker
+            foreach (var cracker in allCrackers)
+                if (cracker.Empty)
+                    return cracker;
+
+            // Fall back to return any cracker
+            foreach (var cracker in allCrackers)
+                return cracker;
 
             return null;
         }
-
-
 
         public static void Postfix(ThingComp __instance, Pawn selPawn, ref IEnumerable<FloatMenuOption> __result)
         {
@@ -53,27 +64,58 @@ namespace Tixiv_BiocodeCracker
                     var cb = getCrackerBuilding(selPawn);
 
                     if (cb != null)
-                    { 
-
+                    {
                         // Create a new list of options including the existing options
                         List<FloatMenuOption> newOptions = new List<FloatMenuOption>(__result);
 
-                        // Add your custom option to the list
-                        newOptions.Add(new FloatMenuOption("Insert in biocode cracker", () =>
+                        if (!cb.Empty)
                         {
-                            // Define what happens when the player clicks the custom option
-                            Log.Message("Custom Biocode Action executed on " + thingWithComps.Label);
-                            // Your custom logic here...
+                            newOptions.Add(new FloatMenuOption("Can't insert in biocode cracker: No empty cracker.", null));
+                        }
+                        else if (!cb.PowerOn)
+                        {
+                            newOptions.Add(new FloatMenuOption("Can't insert in biocode cracker: No powered empty cracker.", null));
+                        }
+                        else if (selPawn.Map.reservationManager.CanReserve(selPawn, thingWithComps) &&
+                                 selPawn.Map.reservationManager.CanReserve(selPawn, cb))
+                        {
+                            string optionString = "Insert " + thingWithComps.Label;
+                            if (thingWithComps.Map.reservationManager.IsReserved(thingWithComps))
+                            {
+                                Pawn reserver;
+                                thingWithComps.Map.reservationManager.TryGetReserver(thingWithComps, null, out reserver);
+                                if (reserver != null)
+                                    optionString += "(reserved by " + reserver.Label + ")";
+                            }
 
+                            optionString += " into biocode cracker";
 
-                            // Create the job for the pawn to use the Biocode Cracker building
-                            Job job = JobMaker.MakeJob(Tixiv_BiocodeCracker_DefOf.InsertInBiocodeCracker, thingWithComps, cb, cb.InteractionCell);
-                            job.count = 1;
+                            if (cb.Map.reservationManager.IsReserved(cb))
+                            {
+                                Pawn reserver;
+                                cb.Map.reservationManager.TryGetReserver(cb, null, out reserver);
+                                if (reserver != null)
+                                    optionString += "(reserved by " + reserver.Label + ")";
 
-                            // Assign the job to the pawn
-                            selPawn.jobs.TryTakeOrderedJob(job);
+                            }
 
-                        }));
+                            // Add your custom option to the list
+                            newOptions.Add(new FloatMenuOption(optionString, () =>
+                            {
+                                // Create the job for the pawn to use the Biocode Cracker building
+                                Job job = JobMaker.MakeJob(Tixiv_BiocodeCracker_DefOf.InsertInBiocodeCracker, thingWithComps, cb, cb.InteractionCell);
+                                job.count = 1;
+
+                                // Assign the job to the pawn
+                                selPawn.jobs.TryTakeOrderedJob(job);
+
+                            }));
+
+                        }
+                        else
+                        {
+                            newOptions.Add(new FloatMenuOption("Can't insert in biocode cracker: No reachable unforbidden cracker.", null));
+                        }
 
                         // Return the modified list of options
                         __result = newOptions;
