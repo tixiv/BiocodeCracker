9a3866dacb6e4008032bd08ba4e9638211d9944c
Peter Fuhrmann <tixiv@gmx.net>
2025-03-23 13:37:42 +0100

    Rewrite cracker timing, save variables

---
+++ Source/BiocodeCracker/UsefullLinks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse.AI;
+using Verse;
+
+namespace Tixiv_BiocodeCracker
+{
+    class UsefullLinks
+    {
+
+        // Just some links to quickly get to the implementation of interesting thing
+        public void links() {
+
+            JobDriver_DisassembleMech dm;
+            JobDriver_HaulMechToCharger hm;
+            JobDriver_InstallRelic ir;
+            JobDriver_CarryToBuilding ctb;
+            JobDriver_EmptyThingContainer etc;
+            JobDriver_FixBrokenDownBuilding jdfbdb;
+            JobDriver_DoBill jddb;
+
+            CompEggContainer cec;
+            CompRelicContainer crc;
+            CompMoteEmitter cme;
+            CompTempControl ctc;
+            CompHeatPusherPowered chpp;
+
+            WorkGiver_HaulToGeneBank wghtg;
+            WorkGiver_Haul wgh;
+
+            MoteAttached ma;
+
+            Building b;
+            Building_GeneExtractor bge;
+            Building_MechGestator bmg;
+
+            ThingDef td;
+
+            CompProperties_ThingContainer tc;
+            CompProperties_Power cpp;
+            CompProperties_TempControl cptc;
+        }
+
+    }
+}
