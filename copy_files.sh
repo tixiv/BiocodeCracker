d3ae833af871660c3628e35088d8a2cff23e26e8
Peter Fuhrmann <tixiv@gmx.net>
2025-05-30 13:49:59 +0200

    Move out of mods Dir and add copy script

---
+++ copy_files.sh
@@ -0,0 +1,6 @@
+cp -r Assemblies /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/TixivBiocodeCracker/
+cp -r Defs /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/TixivBiocodeCracker/
+cp -r About /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/TixivBiocodeCracker/
+cp LICENSE /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/TixivBiocodeCracker/
+
+
