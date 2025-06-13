61a7708478b17556c85bdea1cb36ada46ffd0cac
Peter Fuhrmann <tixiv@gmx.net>
2025-06-13 16:59:09 +0200

    Fix double menu on weapon spawn utility

--- Source/BiocodeCracker/DebugActions.cs
+++ Source/BiocodeCracker/DebugActions.cs
@@ -11,23 +11,14 @@ namespace Tixiv_BiocodeCracker
 {
     public static class DebugActions
     {
-        [DebugAction("Spawning", "Spawn biocoded weapon", false, false, false, false, 0, false, allowedGameStates = AllowedGameStates.PlayingOnMap, displayPriority = 1000)]
-        private static List<DebugActionNode> SpawnBiocodedWeapon()
+        [DebugAction("Spawning", "Spawn biocoded weapon", actionType = DebugActionType.ToolMap, allowedGameStates = AllowedGameStates.PlayingOnMap /* ,hideInSubMenu = true*/)]
+        private static void SpawnBiocodedWeapon()
         {
-            List<DebugActionNode> list = new List<DebugActionNode>();
-
-                list.Add(new DebugActionNode("Spawn biocoded weapon", DebugActionType.ToolMap, null, null)
-                {
-                    action = delegate ()
-                    {
-                        var w = UtilCreateBiocodedWeapon.CreateRandomBiocodedWeapon();
-                        if (w != null)
-                        {
-                            GenSpawn.Spawn(w, UI.MouseCell(), Find.CurrentMap, WipeMode.Vanish);
-                        }
-                    }
-                });
-            return list;
+            var w = UtilCreateBiocodedWeapon.CreateRandomBiocodedWeapon();
+            if (w != null)
+            {
+                GenSpawn.Spawn(w, UI.MouseCell(), Find.CurrentMap, WipeMode.Vanish);
+            }
         }
     }
 }
